bc3860622cacf4570a5c740edb198bbcb3b4cc5c
Belgarion <Collosium@Belgarions-MacBook-Pro.local>
2023-11-11 02:52:01 +0200

    stats at top of sidebar started

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,7 +14,7 @@ export const Sidebar = ({ setCenter }) => {
       >
         <div className="list_item">
           <p>Pair {data.symbol}</p>
-          <p>⏰ Profit {data.profit}</p>
+          <p>Profit $ {data.profit}</p>
         </div>
         <div className="list_item">
           {" "}
@@ -29,6 +29,34 @@ export const Sidebar = ({ setCenter }) => {
   return (
     <div className="sidebar">
       <h2 style={{ textAlign: "center" }}>Orders</h2>
+      <div className="list_item">
+        <p>Num of orders: {data.length}</p>{" "}
+        <p>
+          Profit $ {data.reduce((acc, val) => acc + val.profit, 0).toFixed(2)}
+        </p>
+      </div>
+      <div className="list_item">
+        {" "}
+        <p>
+          Wins of orders: ${" "}
+          {data
+            .filter((data) => data.profit > 0)
+            .reduce((acc, val) => acc + val.profit, 0)
+            .toFixed(2)}
+        </p>
+        <p>
+          Losses of orders: ${" "}
+          {data
+            .filter((data) => data.profit < 0)
+            .reduce((acc, val) => acc + val.profit, 0)
+            .toFixed(2)}
+        </p>
+      </div>
+      <div className="list_item">
+        <p>Num of wins: {data.filter((dat) => dat.profit > 0).length}</p>
+        <p>Num of losses: {data.filter((dat) => dat.profit < 0).length}</p>
+      </div>
+
       <div style={{ overflow: "auto" }}>
         {data.map((data, i) => (
           <MenuItem key={i} data={data} />
